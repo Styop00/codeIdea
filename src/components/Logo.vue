8b95649e8f73aba8623d55a70d0d83703d86e4c6
laurakamalyan <kamalyanlaura095@gmail.com>
2025-01-09 11:27:08 +0400

    change styles

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,3 +1,5 @@
 <template>
-    <a href="#" class="text-logo-color text-[26px]"><span class="font-bold">CODE</span> IDEA</a>
+    <div>
+        <a href="#" class="text-logo-color text-[26px]"><span class="font-bold">CODE</span> IDEA</a>
+    </div>
 </template>
