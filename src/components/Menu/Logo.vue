bc8887ab79f597b83a7924e440ca269ee893de0b
laurakamalyan <kamalyanlaura095@gmail.com>
2025-01-16 14:02:16 +0400

    add logo image

--- src/components/Menu/Logo.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <a href="#" class="text-3xl"><span class="font-bold">CODE</span>IDEA</a>
-    </div>
-</template>
--- src/components/Menu/Sidebar.vue
+++ src/components/Menu/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid md:grid-cols-2 bg-white font-montserrat">
     <div class="bg-black-100 pt-9 h-screen pl-12 pr-10 hidden md:block">
-      <Logo class="text-center md:text-left"/>
+      <img src="@/assets/logo.svg"> 
       <div class="text-3xl md:text-4xl font-bold tracking-wider
       relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <p class="mb-4">Front-end developer</p>
@@ -12,7 +12,7 @@
       </div>
     </div>
     
-    <div class="p-7 md:pt-10">
+    <div class="p-7">
       <div class="text-right">
         <slot/>
       </div>
@@ -28,5 +28,4 @@
 <script setup>
   import SidebarLeftSection from "@/components/Menu/SidebarLeftSection.vue"
   import SidebarMenuItems from "@/components/Menu/SidebarMenuItems.vue"
-  import Logo from "@/components/Menu/Logo.vue"
 </script>
