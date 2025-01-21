8999db4fa47d1030f59de7341f486de102f56017
Koryun <koryunsahakyan10@gmail.com>
2025-01-21 16:44:03 +0400

    changing font size and photo

--- src/components/About/OurMission.vue
+++ src/components/About/OurMission.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex justify-between flex-col md:flex-row lg:flex-row px-5 div_container  gap-12 ">
     <div class=" w-full lg:w-1/2 flex flex-col gap-6">
-      <p class="font-bold text-5xl leading-loose tracking-wider	text-black-250	">Our mission</p>
-      <p class="font-normal text-lg leading-9 text-black-250">Our mission is to build digital products that last. By being pragmatic product experts with a narrow tech specialization, we're able to avoid surprises and deliver highest quality software consistently.</p>
+      <p class="font-bold text-2xl md:text-5xl leading-loose tracking-wider	text-black-250	">Our mission</p>
+      <p class="font-normal text-base md:text-lg leading-9 text-black-250">Our mission is to build digital products that last. By being pragmatic product experts with a narrow tech specialization, we're able to avoid surprises and deliver highest quality software consistently.</p>
 
-      <p class="font-normal text-lg leading-9 text-black-250"> Since 2015, we have successfully completed 80+ web, mobile and desktop projects in fintech, education, healthcare and other industries, working with brands like National Geographic, Beam or Paradox Interactive.</p>
+      <p class="font-normal text-base md:text-lg leading-9 text-black-250"> Since 2015, we have successfully completed 80+ web, mobile and desktop projects in fintech, education, healthcare and other industries, working with brands like National Geographic, Beam or Paradox Interactive.</p>
 
-      <p class="font-normal text-lg leading-9 text-black-250"> Our approach is simple: build partnerships with our clients where we work together towards an optimal solution for their business. </p>
+      <p class="font-normal text-base md:text-lg leading-9 text-black-250"> Our approach is simple: build partnerships with our clients where we work together towards an optimal solution for their business. </p>
     </div>
     <div class="w-full lg:w-1/2 ">
         <div class="w-full relative bg-gray-100 h-72 z-1">
