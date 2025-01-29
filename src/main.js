c3f4aecd8b5b80ee66281732aed834ed87eab7d6
Koryun <koryunsahakyan10@gmail.com>
2025-01-29 13:59:00 +0400

    removed unused packeges

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
 import './assets/main.css'
 import VCalendar from 'v-calendar';
-import Multiselect from 'vue-multiselect'
 import 'v-calendar/style.css';
 import VueTelInput from 'vue-tel-input';
 import 'vue-tel-input/vue-tel-input.css';
-
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 library.add(fas);
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
@@ -20,25 +17,16 @@ import {createRouter, createWebHistory} from 'vue-router'
 import {createPinia} from 'pinia';
 import routes from "./router/routes";
 const router = createRouter({ history: createWebHistory(), routes });
-
 const pinia = createPinia();
 const globalOptions = {
   mode: 'auto',
 };
-
-
-
-
-
 const app = createApp(App)
-
 app.use(router)
 app.component('fa', FontAwesomeIcon)
 app.use(pinia)
 app.use(ElementPlus);
-
 app.use(VueTelInput,globalOptions);
-app.component('Multiselect', Multiselect)
 app.mount('#app')
 app.use(VCalendar, {})
 
