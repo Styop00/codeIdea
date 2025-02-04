67e7cdbf3bbc6979d9ba2f38e7908a592e4ae3ff
Koryun <koryunsahakyan10@gmail.com>
2025-02-04 11:29:47 +0400

    fix Technologies page problems

--- src/components/ServicePage/ServiceTechnologies/Technologies.vue
+++ src/components/ServicePage/ServiceTechnologies/Technologies.vue
@@ -4,8 +4,7 @@
       <div class="w-1/4 h-1 md:hidden bg-black  "></div>
     <div class=" lg:gap-16 gap-3 flex flex-wrap  px-5 lg:px-0 md:px-0 md:gap-14">
       <p v-for="logo in data" class="w-11 lg:w-20 md:w-20">
-        <img :src="logo" class=" transition-ease duration-300 grayscale	 hover:grayscale-0 " v-if="logo!==iosLogo" >
-        <img :src="logo" class=" transition-ease duration-500 invert	hover:invert-0" v-else>
+        <img :src="logo" :class=" logo!=='/src/images/ios.png'?'transition-ease duration-300 grayscale hover:grayscale-0':'transition-ease duration-1000 invert hover:invert-0' "  >
       </p>
 
     </div>
